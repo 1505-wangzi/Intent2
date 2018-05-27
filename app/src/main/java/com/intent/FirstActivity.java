875df393e25055079d082e70ecf8ad7ec40a27b9
1505-wangzi <1046925459@qq.com>
2018-05-27 14:27:55 +0800

    the last one

---
+++ app/src/main/java/com/intent/FirstActivity.java
@@ -0,0 +1,26 @@
+package com.intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.intent.ACTION_START");
+                intent.addCategory("com.intent.MY_CATEGORY");
+                startActivity(intent);
+            }
+        });
+    }
+}
